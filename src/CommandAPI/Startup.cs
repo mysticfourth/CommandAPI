bb4f3a468aa824c4e0bd3e2fecedb31296ffbada
sajiths <sajithsasi@outlook.com>
2021-12-02 06:18:33 +0530

    Environment Variable & User Secrets

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace CommandAPI
 {
@@ -26,10 +27,13 @@ namespace CommandAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+            
             services.AddDbContext<CommandContext>(
-                opt => opt.UseNpgsql
-                (Configuration.GetConnectionString("PostgreSqlConnection"))
-            );
+                opt => opt.UseNpgsql(builder.ConnectionString));
 
             services.AddControllers();
 
