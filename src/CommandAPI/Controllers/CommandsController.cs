516110f135170fa7e090de40631b93eef28f8b26
sajiths <sajithsasi@outlook.com>
2021-12-20 14:54:53 +0530

    Authentication details

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -5,6 +5,7 @@ using CommandAPI.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CommandAPI.Controllers
 {
@@ -27,6 +28,7 @@ namespace CommandAPI.Controllers
             return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
         }
 
+        [Authorize]
         [HttpGet("{id}", Name = "GetCommandById" )]
         public ActionResult<CommandReadDto> GetCommandById(int id)
         {
--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using Npgsql;
 using AutoMapper;
 using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace CommandAPI
 {
@@ -37,6 +38,13 @@ namespace CommandAPI
             services.AddDbContext<CommandContext>(
                 opt => opt.UseNpgsql(builder.ConnectionString));
 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt =>
+                {
+                    opt.Audience = Configuration["ResourceId"];
+                    opt.Authority = $"{Configuration["Instance"]}{Configuration["TenantId"]}";
+                });
+
             services.AddControllers().AddNewtonsoftJson(s => {
                 s.SerializerSettings.ContractResolver = new
                 CamelCasePropertyNamesContractResolver();
@@ -59,6 +67,9 @@ namespace CommandAPI
             }
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
