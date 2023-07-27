88e05e4770e427e416553c07204d3a154de936a9
sajiths <sajithsasi@outlook.com>
2023-07-27 12:52:41 +0530

    Secured our connection string

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,9 +1,25 @@
 using CommandAPI.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+var builderData = new SqlConnectionStringBuilder();
+
+builderData.ConnectionString = builder.Configuration.GetConnectionString("SqlServerSqlConnection");
+
+
+builderData.UserID = builder.Configuration["UserID"];
+builderData.Password = builder.Configuration["Password"];
+
+builder.Services.AddDbContext<CommandContext>(opt => opt.UseSqlServer
+(builderData.ConnectionString));
+
 builder.Services.AddControllers();
-builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+builder.Services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
 
 var app = builder.Build();
 
