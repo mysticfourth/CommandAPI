9da3a404b8e9d7e0ca792df46ca2f3cff8688be3
sajiths <sajithsasi@outlook.com>
2023-07-29 23:54:06 +0530

    Added Create Method changes

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -32,7 +32,7 @@ namespace CommandAPI.Controllers
             return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetCommandById")]
         public ActionResult<CommandReadDto> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
@@ -50,8 +50,7 @@ namespace CommandAPI.Controllers
             _repository.CreateCommand(commandModel);
             _repository.SaveChanges();
             var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
-            return CreatedAtRoute(nameof(GetCommandById),
-            new { Id = commandReadDto.Id }, commandReadDto);
+            return CreatedAtRoute(nameof(GetCommandById), new { Id = commandReadDto.Id }, commandReadDto);
         }
     }
 }
